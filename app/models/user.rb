11b0edbc415c65877d13d4aafc10f7c7bceefb5a
Antony Adhiban & Gina Benavidez <pair+antonyadhiban.gbenavid@devbootcamp.com>
2017-04-29 05:37:40 -0700

    Create association for #Tag: A #Tag belongs to an #Article, and a #User can create many #Tags through #Articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
   has_many :friends, class_name: :"User", foreign_key: :"friends_id"
   belongs_to :friend, class_name: "User"
   has_many :articles
+  has_many :tags, through: :articles
 
 end
