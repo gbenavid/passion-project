e9e2bcd81eb36bb269fef7c5d031049948f5f2d4
[01;31m[K"name"[m[K: "Gina Benavidez <[01;31m[K"email"[m[K: null>
2017-04-30 00:59:36 -0700

    rearrange file configuration

--- app/controllers/users/users_index.rb
+++
@@ -1,8 +0,0 @@
-get '/' do 
-	erb :'layout'
-end
-
-# User is able to create an account (sign up)
-# User is able to view their own facebook profile
-# User is able to view other users' profiles
-# User is able to edit only their own profile
